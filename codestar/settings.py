240b0deb39a4d8d6fbd79a250da084255a89d318
Technoid <Technoid@Sazonov.localdomain>
2024-02-01 19:35:40 +0000

    Test secret key

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lb$!&em=)g6deeymd1i7clxha=!kbjf0#&3(6^)6x1$!(n2$!*'
-
+#SECRET_KEY = 'django-insecure-lb$!&em=)g6deeymd1i7clxha=!kbjf0#&3(6^)6x1$!(n2$!*'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 
 # Application definition
