9d424a43778a081ea7885978084967dcac2b9bc9
Technoid <Technoid@Sazonov.localdomain>
2024-02-05 14:45:02 +0000

    Add comments

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 #SECRET_KEY = 'django-insecure-lb$!&em=)g6deeymd1i7clxha=!kbjf0#&3(6^)6x1$!(n2$!*'
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
